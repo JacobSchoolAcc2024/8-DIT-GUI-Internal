81c96c25fcd3f541725f9ad82c77ece14fd638d6
Jacob T <21btorcino@wakatipu.school.nz>
2025-05-14 15:06:09 +1200

    Added functionality to all buttons and upgrades.
    Finished base game

--- 8_Dit_Gui_Internal.py
+++ 8_Dit_Gui_Internal.py
@@ -20,11 +20,16 @@ class GameLogic:
         self.baseMakeSpeed = 5
         self.baseProductGain = 1
         self.productValue = 5
+        self.productValueMulti = 1 
 
         self.sellProduct1Requirement = 1
         self.sellProductSpeed = 5
 
         self.upgradeOreRequirement = 5
+        self.upgradeProductValueRequirement = 10
+
+        self.combineProductRequirement = 3
+        self.combineProductSpeed = 10
     
     def validate(self,current,required):
         if current >= required:
@@ -35,8 +40,12 @@ class GameLogic:
     def updateGUi(self):
         self.gui.oreLabel.config(text = f'Ores: {self.ores}')
         self.gui.productLabel.config(text = f'Product Made: {self.product1}')
-        self.gui.moneyLabel.config(text = f'Money: {self.money}')
+        self.gui.moneyLabel.config(text = f'Money: ${self.money}')
         self.gui.oreGainLabel.config(text = f'Ore Gain: {self.baseOreGain}')
+        self.gui.productValueLabel.config(text = f'Product Value: ${float(self.productValue*self.productValueMulti)}')
+        self.gui.productValueMultiLabel.config(text = f'Product Value Multiplier: {float(self.productValueMulti)}')
+
+        self.gui.oreUpgrade.config(text = f'(${self.upgradeOreRequirement}) Upgrade Ores')
 
     def start_timer(self, timer,label):
         for i in range(timer, -1, -1):
@@ -48,7 +57,7 @@ class GameLogic:
             btn.config(state="disabled")
             self.start_timer(speed,label)
             btn.config(state="normal")
-            label.config(text=f'Wait {speed}')
+            label.config(text=f'')
             self.updateGUi()
             if after_func:
                 after_func()
@@ -86,7 +95,8 @@ class GameLogic:
         okay = self.validate(self.product1,self.sellProduct1Requirement)
         selling = self.product1 * self.sellProduct1Requirement
         def finishedSelling():
-            self.money += selling * self.productValue
+            self.money += selling * (self.productValue * self.productValueMulti)
+            self.productValue = 5 * self.productValueMulti
             self.updateGUi()
         if okay == True:
             pass
@@ -97,7 +107,7 @@ class GameLogic:
             self.product1 -= selling
             self.updateGUi()
         else:
-            messagebox.showerror("Insufficient product", f"You need atleast {self.sellProduct1Requirement} product to sell")
+            messagebox.showerror("Insufficient product", f"You need {self.sellProduct1Requirement} product to sell")
 
     def upgradeOres(self):
         okay = self.validate(self.money,self.upgradeOreRequirement)
@@ -105,11 +115,39 @@ class GameLogic:
         if okay == True:
             self.money -= self.upgradeOreRequirement
             self.baseOreGain += 1
-            self.upgradeOreRequirement += 1
-            messagebox.showinfo("Upgrade Successful", f"You now gain {self.baseOreGain} ores per mine")
+            self.upgradeOreRequirement += 10
             self.updateGUi()
         else:
-            messagebox.showerror("Insufficient money", f"You need atleast ${buying}")
+            messagebox.showerror("Insufficient money", f"You need ${float(buying)}")
+
+    def upgradeProductValue(self):
+        okay = self.validate(self.money,self.upgradeProductValueRequirement)
+        buying = self.upgradeProductValueRequirement - self.money
+        if okay == True:
+            self.money -= self.upgradeProductValueRequirement
+            self.productValueMulti += 0.5
+            self.upgradeProductValueRequirement += 20
+            self.updateGUi()
+        else:
+            messagebox.showerror("Insufficient money", f"You need ${float(buying)}")
+    
+    def combineProduct(self):
+        okay = self.validate(self.product1,self.combineProductRequirement)
+        combining = self.combineProductRequirement - self.product1
+
+        def finishedCombining():
+            self.productValue += 20
+            self.updateGUi()
+
+        if okay == True:
+            btn = self.gui.combineButton
+            speed = self.combineProductSpeed
+            label = self.gui.combineButtonLabel
+            self.start(btn,speed,label,after_func=finishedCombining)
+            self.product1 -= self.combineProductRequirement
+            self.updateGUi()
+        else:
+            messagebox.showerror("Insufficient Products", f"You need {combining} products to perform combination")
 
         
 class GameGUI:
@@ -124,10 +162,10 @@ class GameGUI:
             root.grid_rowconfigure(i+1, weight=1)
             
         statusFrame = tk.Frame(root, bg="grey")
-        statusFrame.grid(row=0, column=0, columnspan=4, rowspan=1, sticky="ew", padx=5, pady=5)
+        statusFrame.grid(row=0, column=0, columnspan=4, sticky="ew", padx=5, pady=5)
         for i in range(4):
             statusFrame.grid_columnconfigure(i, weight=1)
-
+  
         creationFrame = tk.Frame(root, bg="black")
         creationFrame.grid(row=1, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
         for i in range(2):
@@ -143,21 +181,26 @@ class GameGUI:
 
         
         ############################################################
+
         self.moneyLabel = tk.Label(statusFrame, text="Money: $0", font=('Arial', 20),
                  fg="yellow", bg='grey')
         self.moneyLabel.grid(row=0, column=0, sticky="w")
+
+        self.productValueLabel = tk.Label(statusFrame, text="Product Value: $5.0", font=('Arial', 20),
+                 fg="yellow", bg='grey')
+        self.productValueLabel.grid(row=1, column=0, sticky="w")
+
+        self.productValueMultiLabel = tk.Label(statusFrame, text="Product Value Multiplier: 1.0", font=('Arial', 20),
+                 fg="yellow", bg='grey')
+        self.productValueMultiLabel.grid(row=2, column=0, sticky="w")
         
         self.oreLabel = tk.Label(statusFrame, text="Ores: 0", font=('Arial', 20),
                  fg="white", bg='grey')
         self.oreLabel.grid(row=0, column=1)
 
-        self.oreGainLabel = tk.Label(statusFrame, text="Ore Gain: 1", font=('Arial', 18),
+        self.oreGainLabel = tk.Label(statusFrame, text="Ore Gain: 1", font=('Arial', 20),
                  fg="white", bg='grey')
         self.oreGainLabel.grid(row=1, column=1)
-        
-        self.strengthLabel = tk.Label(statusFrame, text="Strength: 0", font=('Arial', 20),
-                 fg='red', bg='grey')
-        self.strengthLabel.grid(row=0, column=2, sticky="e")
 
         self.productLabel = tk.Label(statusFrame, text="Product Made: 0", font=('Arial', 20),
                                      fg='white', bg='grey')
@@ -171,9 +214,13 @@ class GameGUI:
                  fg='white', bg='black')
         self.sellWait.grid(row=1, column=0, sticky="n", padx=10)
 
-        self.CombineButton = tk.Button(creationFrame, text="Combine Product", font=('Arial', 20),
+        self.combineButton = tk.Button(creationFrame, text="Combine Product", font=('Arial', 20),
                   fg='yellow', bg='black', width=15)
-        self.CombineButton.grid(row=2, column=0, sticky="ew", padx=10, pady=10)
+        self.combineButton.grid(row=2, column=0, sticky="ew", padx=10, pady=10)
+
+        self.combineButtonLabel = tk.Label(creationFrame, text="", font=('Arial', 16),
+                 fg='white', bg='black')
+        self.combineButtonLabel.grid(row=3, column=0, sticky="n", padx=10)
 
         self.mineButton = tk.Button(creationFrame, text="Mine Ores", font=('Arial', 20),
                   fg='red', bg='black', width=15)
@@ -190,23 +237,20 @@ class GameGUI:
         self.makeWait = tk.Label(creationFrame, text="", font=('Arial', 16),
                  fg='white', bg='black')
         self.makeWait.grid(row=3, column=1, sticky="n", padx=10)
+
         ############################################################
 
         self.oreUpgrade = tk.Button(upgradeFrame, text="($5) Upgrade Ores", font=('Arial', 20),
                   fg='white', bg='grey', width=15)
         self.oreUpgrade.grid(row=0, column=1, sticky="ew", padx=10, pady=10)
-        Hovertip(self.oreUpgrade, "Increases ores gained from mining by 1")
+        Hovertip(self.oreUpgrade, "Increases ores gained from mining by 1", hover_delay=100)
 
         self.productUpgrade = tk.Button(upgradeFrame, text="($10) Upgrade Product Value", font=('Arial', 20),
                   fg='white', bg='grey', width=15)
         self.productUpgrade.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
+        Hovertip(self.productUpgrade,"Increases product value by 2", hover_delay=100)
 
         
-
-        
-
-
-
 class GuiController:
     def __init__(self,gui,logic):
         self.gui = gui
@@ -223,8 +267,13 @@ class GuiController:
 
     def handleOreUpgrade(self):
         self.logic.upgradeOres()
+    
+    def handleCombination(self):
+        self.logic.combineProduct()
+    
+    def handleProductValueUpgrade(self):
+        self.logic.upgradeProductValue()
         
-
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -239,6 +288,8 @@ if __name__ == "__main__":
     gui.makeButton.config(command=controller.handleProduct)
     gui.sellButton.config(command=controller.handleSelling)
     gui.oreUpgrade.config(command=controller.handleOreUpgrade)
+    gui.combineButton.config(command=controller.handleCombination)
+    gui.productUpgrade.config(command=controller.handleProductValueUpgrade)
 
     root.mainloop()
 
